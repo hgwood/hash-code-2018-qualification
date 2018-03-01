660b7d7437f8f876ac8c5e0c9a0d28ec50f864d8
Thibaud Courtoison <do.not.press.enter@gmail.com>
2018-03-01 22:27:55 +0100

    'tayo!'

--- sort.js
+++ sort.js
@@ -4,13 +4,13 @@ const _ = require("lodash");
 
 function sortRides(rides, nrides, time) {
   if (time > 30000) {
-    rides = _.orderBy(rides, ["distance"], ["desc"]);
+    //rides = _.orderBy(rides, ["distance"], ["desc"]);
     min = _.min(rides, "distance");
     rides = rides.filter(r => r.distance == min.distance);
     return _.orderBy(rides, ["distanceToV", "start"], ["asc", "asc"]);
   } else {
-    rides = rides.filter(r => r.distance <= 1000);
-    rides = _.orderBy(rides, ["distanceToV"], ["asc"]);
+    rides = rides.filter(r => r.distance <= 5000);
+    //rides = _.orderBy(rides, ["distanceToV"], ["asc"]);
     min = _.min(rides, "distanceToV");
     rides = rides.filter(r => r.distanceToV == min.distanceToV);
     return _.orderBy(rides, ["start", "distance"], ["asc", "desc"]);
