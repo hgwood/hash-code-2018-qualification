427a371c0634f2664a8f2311e65accb06fa37afc
Hugo Wood <hx4.5@free.fr>
2018-03-01 19:47:50 +0100

    fix sort by longest distance

--- sort.js
+++ sort.js
@@ -3,7 +3,7 @@ const debug = require("debug")("sort");
 const _ = require("lodash");
 
 function sortRides(rides) {
-  return _.sortBy(rides, ["start", "distance"]); // distance
+  return _.orderBy(rides, ["start", "distance"], ["asc", "desc"]);
 }
 
 module.exports = { sortRides };
