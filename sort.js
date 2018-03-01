4eabe3e44406d57a3a58dcda32802f4a09735574
Thibaud Courtoison <do.not.press.enter@gmail.com>
2018-03-01 20:29:48 +0100

    'tayo!'

--- sort.js
+++ sort.js
@@ -5,7 +5,7 @@ const _ = require("lodash");
 function sortRides(rides, nrides) {
   return _(rides)
     .orderBy(["distanceToV"], ["asc"])
-    .take(nrides / 10)
+    .take(Math.max(100, nrides / 10))
     .orderBy(["start", "distance"], ["asc", "desc"])
     .value();
 }
