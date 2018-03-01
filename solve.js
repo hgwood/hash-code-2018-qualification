1ac70071b4c68e8f421739aef9fe8e3cc8b79822
Hugo Wood <hx4.5@free.fr>
2018-03-01 18:59:43 +0100

    jsdoc for inputs and outputs of solve

--- solve.js
+++ solve.js
@@ -2,8 +2,30 @@ const debug = require("debug")("solve");
 const _ = require("lodash");
 const gridUtils = require("./grid-utils");
 
+/**
+ * @typedef {object} Ride
+ * @property {number} ox
+ * @property {number} oy
+ * @property {number} dx
+ * @property {number} dy
+ * @property {number} start
+ * @property {number} finish
+ *
+ * @typedef {object} Problem
+ * @property {number} nrows
+ * @property {number} ncolumns
+ * @property {number} nbonus
+ * @property {number} nsteps
+ * @property {Ride[]} rides
+ *
+ * @typedef {number[]} Solution
+ * @property {number}
+ *
+ * @param {Problem} problem
+ * @returns {Solution[]}
+ */
 function solve(problem) { // destructure this!
-  return [];
+  return [[1,2],[3,4]];
 }
 
 module.exports = solve;
